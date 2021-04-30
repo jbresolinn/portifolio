1388b38aa80c08c7d1964314db085b219350a610
julia <juliabresolin2@gmail.com>
2021-04-29 22:15:43 -0300

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>início |&nbsp; julia bresolin</title>
+      </Head>
+      <main>
+        <h1>hello world</h1>
+      </main>
+    </>
+  )
+}
